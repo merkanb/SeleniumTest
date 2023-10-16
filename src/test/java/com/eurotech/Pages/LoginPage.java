9f05c6974a512c3d379dbe3c0773742fd0db5a1a
merkanb <merkanb@gmail.com>
2023-10-17 00:24:34 +0200

    loginpage added

---
+++ src/test/java/com/eurotech/Pages/LoginPage.java
@@ -0,0 +1,68 @@
+package com.eurotech.Pages;
+
+import com.eurotech.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+
+
+public class LoginPage extends BasePage{
+
+    @FindBy(id = "loginpage-input-email")
+    public WebElement usernameInput;
+
+    @FindBy(css = "#loginpage-form-pw-input")
+    public WebElement passwordInput;
+
+    @FindBy(id = "loginpage-form-btn")
+    public WebElement loginBtn;
+
+    @FindBy(id = "rcc-confirm-button")
+    public WebElement understandBtn;
+
+    @FindBy(xpath = "//a[contains(text(),'Forgot')]")
+    public WebElement forgotPasswordBtn;
+
+    @FindBy(xpath = "//*[text()='Invalid Credentials!']")
+    public WebElement warningMessage;
+
+    //@FindAll
+    //To use multiple locator if at least one of them is matching it will find the web element.     ONE MATCHES OK !!!
+    @FindAll({
+            @FindBy(id = "loginpage-input-email"), //or  --> logical OR
+            @FindBy(name = "emailhghjghjghghghjgjh")})
+    public WebElement usernameInputFindAll;
+
+
+    //@FindBys
+    //if the locators are matching with the element then it will return it.    BOTH SHOULD MATCH !!!
+    @FindBys({
+            @FindBy(id = "loginpage-form-pw-input"),   // and   --> logical AND
+            @FindBy(name = "password")
+    })
+    public WebElement passwordInputFindBys;
+
+
+
+    public void login(String username, String password) {
+        understandBtn.click();
+        usernameInput.sendKeys(username);
+        passwordInput.sendKeys(password);
+        loginBtn.click();
+    }
+
+    public void loginWithStudent() {
+        understandBtn.click();
+        usernameInput.sendKeys(ConfigurationReader.get("usernameStudent"));
+        passwordInput.sendKeys(ConfigurationReader.get("passwordStudent"));
+        loginBtn.click();
+    }
+
+    public void loginWithTeacher() {
+        understandBtn.click();
+        usernameInput.sendKeys(ConfigurationReader.get("usernameTeacher"));
+        passwordInput.sendKeys(ConfigurationReader.get("passwordTeacher"));
+        loginBtn.click();
+    }
+}
