2a2950eb1e194be99e83bf7e03b90891d9c98791
merkanb <merkanb@gmail.com>
2023-10-17 00:25:06 +0200

    basepage added

---
+++ src/test/java/com/eurotech/Pages/BasePage.java
@@ -0,0 +1,18 @@
+package com.eurotech.Pages;
+
+import com.eurotech.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+public abstract class BasePage {                // it is abstract class !!
+
+public BasePage(){
+    PageFactory.initElements(Driver.get(),this);
+}
+
+@FindBy(xpath = "(//span[@class='hide-sm'])[1]")
+public static WebElement logout;
+
+}
