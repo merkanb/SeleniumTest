91508dd21b46385c78ce008f726fc0b2a0b0c589
merkanb <merkanb@gmail.com>
2023-10-17 14:15:39 +0200

    BrowserUtils tests are added

---
+++ src/test/java/com/eurotech/tests/day16_POM2/BrowserUtilsTest.java
@@ -0,0 +1,48 @@
+package com.eurotech.tests.day16_POM2;
+
+import com.eurotech.tests.TestBase;
+import com.eurotech.utilities.BrowserUtils1Me;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class BrowserUtilsTest extends TestBase {
+
+        @Test
+        public void window() {
+
+                driver.get("https://the-internet.herokuapp.com/windows");
+
+// get title
+// Click "Click Here" link
+// get title of the new window
+
+                String titleBeforeClick = driver.getTitle();
+                System.out.println("titleBeforeClick = " + titleBeforeClick);
+
+                driver.findElement(By.linkText("Click Here")).click();
+                BrowserUtils1Me.switchToWindow("New Window");
+                System.out.println("driver.getTitle() = " + driver.getTitle());
+        }
+        @Test
+        public void action() {
+                driver.get("https://the-internet.herokuapp.com/hovers");
+
+                WebElement img1 = driver.findElement(By.xpath("(//img)[2]"));
+
+                BrowserUtils1Me.hover(img1);
+        }
+
+
+        @Test
+        public void list() {
+                driver.get("https://the-internet.herokuapp.com/");
+
+                List<WebElement> menuList = driver.findElements(By.xpath("//a[text()='Broken Images']/../../li"));
+
+                System.out.println("BrowserUtils.getElementsText(menuList) = " + BrowserUtils1Me.getElementsText(menuList));
+        }
+
+}
