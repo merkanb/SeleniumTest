5d23181633d3d456b3e5a085051211d363b7d503
merkanb <merkanb@gmail.com>
2023-10-18 12:43:45 +0200

    Dashboard tests and Dashboard page are added

---
+++ src/test/java/com/eurotech/Pages/DashboardPage.java
@@ -0,0 +1,60 @@
+package com.eurotech.Pages;
+
+import com.eurotech.utilities.BrowserUtils1Me;
+import com.eurotech.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import java.util.List;
+
+public class DashboardPage extends BasePage{
+
+    @FindBy(css="#dashboard-p1")
+    public WebElement welcomeMessage;
+
+    @FindBy(css=".nav__menu-item")
+    public List<WebElement> menulist;           // Developers       All Posts       My Account
+
+    @FindBy(linkText = "All Posts")
+    public WebElement allPost;
+
+    @FindBy(linkText = "Developers")
+    public WebElement developers;
+
+    @FindBy(xpath = "//span[text()='My Account']")
+    public WebElement myAccount;
+
+    @FindBy(xpath = "//a[@class='btn btn-light']")
+    public List<WebElement> dashboardList;          //    Edit Profile     Add Experience    Add Education
+
+    ////tbody/tr[2]/td[2]
+    @FindBy(xpath = "//td[@class='hide-sm'][1]")
+    public WebElement jobTitle;
+
+        @FindBy(xpath = "//td[@class='hide-sm']/preceding-sibling::td[1]")
+//    @FindBy(xpath = "//td[@class='hide-sm']/following-sibling::td[1]")
+    public WebElement companyName;
+
+
+    public void navigateMenu(String menuName){
+
+        Driver.get().findElement(By.xpath("//*[text()='"+menuName+"']")).click();
+
+    }
+    public void navigateSubMenu(String subMenuName){
+
+        BrowserUtils1Me.hover(myAccount);
+        Driver.get().findElement(By.xpath("//span[text()='"+subMenuName+"']")).click();
+    }
+
+    public void verifyTitle(String title){
+        Assert.assertEquals(jobTitle.getText(),title);
+    }
+    public void verifyCompany(String company){
+        Assert.assertEquals(companyName.getText(),company);
+    }
+
+
+}
---
+++ src/test/java/com/eurotech/tests/day16_POM2/DashboardTest.java
@@ -0,0 +1,65 @@
+package com.eurotech.tests.day16_POM2;
+
+import com.eurotech.Pages.DashboardPage;
+import com.eurotech.Pages.LoginPage;
+import com.eurotech.tests.TestBase;
+import com.eurotech.utilities.BrowserUtils1Me;
+import com.eurotech.utilities.ConfigurationReader;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class DashboardTest extends TestBase {
+    LoginPage loginPage=new LoginPage();
+    DashboardPage dashboardPage=new DashboardPage();
+    @Test
+    public void testName() {
+
+        /*
+Navigate to http://eurotech.study/login
+Login with teacher credentials
+Get the text of the dashboard menu list
+Verify that menu list is
+Developers
+All Posts
+My Account
+*/
+        driver.get(ConfigurationReader.get("url"));
+        loginPage.loginWithTeacher();
+        String expectedText= "Welcome Teacher";
+//        Assert.assertEquals(dashboardPage.welcomeMessage.getText(),"Welcome Teacher");
+        Assert.assertEquals(dashboardPage.welcomeMessage.getText(),expectedText);
+
+
+//        ArrayList<String> expectedList=new ArrayList<>();
+//        expectedList.add("Developers");
+//        expectedList.add("All Posts");
+//        expectedList.add("My Account");
+        ArrayList<String> expectedList=new ArrayList<>(Arrays.asList("Developers", "All Posts", "My Account"));
+
+        //1. way
+        ArrayList<String> actualList=new ArrayList<>();
+        List<WebElement> dashboardMenu=dashboardPage.menulist;
+        for (WebElement menu : dashboardMenu) {
+//            System.out.println("menu.getText() = " + menu.getText());
+            actualList.add(menu.getText());
+        }
+        System.out.println("actualList = " + actualList);
+        Assert.assertEquals(actualList,expectedList);
+
+        // 2. way
+        List<String>actualList1= BrowserUtils1Me.getElementsText(dashboardPage.menulist);
+        Assert.assertEquals(actualList1,expectedList);
+        System.out.println("actualList1 = " + actualList1);
+
+        // 3. way
+        Assert.assertEquals(BrowserUtils1Me.getElementsText(dashboardPage.menulist), expectedList);
+
+    }
+
+
+}
