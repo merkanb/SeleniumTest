fe8683562b1339c51165b335a77ac58bc6745650
merkanb <merkanb@gmail.com>
2023-10-31 14:36:07 +0100

    Dashboard uploaded

--- src/test/java/com/eurotech/tests/day16_POM2/DashboardTest.java
+++ src/test/java/com/eurotech/tests/day16_POM2/DashboardTest.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+
 public class DashboardTest extends TestBase {
     LoginPage loginPage=new LoginPage();
     DashboardPage dashboardPage=new DashboardPage();
