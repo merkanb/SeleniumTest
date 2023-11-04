ad05c2a70dbabf0c7f23147de8cc2c10bb6c4009
merkanb <merkanb@gmail.com>
2023-11-04 16:53:06 +0100

    Driver class updated

---
+++ src/test/java/com/eurotech/tests/day15_POM1/PositiveLoginTest.java
@@ -0,0 +1,63 @@
+package com.eurotech.tests.day15_POM1;
+
+import com.eurotech.Pages.LoginPage;
+import com.eurotech.tests.TestBase;
+import com.eurotech.utilities.BrowserUtils1Me;
+import com.eurotech.utilities.ConfigurationReader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PositiveLoginTest extends TestBase {
+    LoginPage loginPage=new LoginPage();
+
+    @Test
+    public void positiveLogin() throws InterruptedException {
+
+        driver.get(ConfigurationReader.get("url"));
+        loginPage.understandBtn.click();
+        loginPage.usernameInput.sendKeys(ConfigurationReader.get("usernameTeacher"));
+        loginPage.passwordInput.sendKeys(ConfigurationReader.get("passwordTeacher"));
+        loginPage.loginBtn.click();
+
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://eurotech.study/dashboard");
+    }
+
+    @Test
+    public void loginWithStudent() {
+        driver.get(ConfigurationReader.get("url"));
+        BrowserUtils1Me.waitForVisibility(loginPage.understandBtn,15);
+        loginPage.understandBtn.click();
+        loginPage.usernameInput.sendKeys(ConfigurationReader.get("usernameStudent"));
+        loginPage.passwordInput.sendKeys(ConfigurationReader.get("passwordStudent"));
+//        loginPage.loginBtn.click();
+        BrowserUtils1Me.clickWithJS(loginPage.loginBtn);
+
+    }
+
+    @Test
+    public void loginWithMethod() {
+        driver.get(ConfigurationReader.get("url"));
+//        loginPage.login(ConfigurationReader.get("usernameStudent"), ConfigurationReader.get("passwordStudent"));
+        // login() created in Login class
+        String username=ConfigurationReader.get("usernameStudent");
+        String password=ConfigurationReader.get("passwordStudent");
+        loginPage.login(username,password);
+    }
+
+    @Test
+    public void loginWithStudent2() throws InterruptedException {
+        driver.get(ConfigurationReader.get("url"));
+        loginPage.loginWithStudent();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://eurotech.study/dashboard");
+    }
+
+    @Test
+    public void loginWithTeacher() throws InterruptedException {
+        driver.get(ConfigurationReader.get("url"));
+        loginPage.loginWithTeacher();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.getCurrentUrl(), "http://eurotech.study/dashboard");
+    }
+}
