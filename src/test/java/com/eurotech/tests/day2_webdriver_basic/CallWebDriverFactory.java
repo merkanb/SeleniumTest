e39d5d1d0f52283d0164bf0ba8f083d1cdcd7680
merkanb <merkanb@gmail.com>
2024-02-02 00:00:27 +0100

    CALL web updated,

--- src/test/java/com/eurotech/tests/day2_webdriver_basic/CallWebDriverFactory.java
+++ src/test/java/com/eurotech/tests/day2_webdriver_basic/CallWebDriverFactory.java
@@ -9,6 +9,7 @@ public class CallWebDriverFactory {
         TC        navigates to http://eurotech.study/login        verify that title is DevEx
          */
 
+
         WebDriver driver= WebDriverFactory.getDriver("chRomE");
         driver.get("http://www.eurotech.study/login");
 
