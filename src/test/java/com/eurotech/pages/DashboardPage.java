c2dce672499e173e607dcef0dbdff506185535e3
merkanb <merkanb@gmail.com>
2024-03-22 16:52:05 +0100

    CALL web updated,

--- src/test/java/com/eurotech/pages/DashboardPage.java
+++ src/test/java/com/eurotech/pages/DashboardPage.java
@@ -37,7 +37,6 @@ public class DashboardPage extends BasePage{
 //    @FindBy(xpath = "//td[@class='hide-sm']/following-sibling::td[1]")
     public WebElement companyName;
 
-
     public void navigateMenu(String menuName){
 
         Driver.get().findElement(By.xpath("//*[text()='"+menuName+"']")).click();
